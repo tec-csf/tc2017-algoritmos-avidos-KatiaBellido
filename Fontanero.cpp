805e535ab1f5d4972585c113d7bbac6d6e1ec956
Katia  Bellido <A01023638@itesm.mx>
2020-03-29 16:28:19 -0600

    Fontanero completo

--- Fontanero.cpp
+++ Fontanero.cpp
@@ -22,14 +22,17 @@ class Fontanero
 };
 class Satisfaccion
 {
-
+    Satisfaccion(){}
     public:
-        int jornada, fontanero;
+        int jornada, sati;
         vector<Fontanero> solucion;
-        Satisfaccion(){}
+        Satisfaccion(int _sati){
+            sati = _sati;
+        }
         void mejorarSatis(vector<Fontanero> trabajo)
         {
             int tiempoDeEspera = 0;
+            int clientesSati = 0;
             for (Fontanero f : trabajo)
             {
                 int temp = tiempoDeEspera + f.llegar;
@@ -41,12 +44,29 @@ class Satisfaccion
             jornada = tiempoDeEspera;
 
             cout<<"Tabla satisfaccion clientes:"<< solucion.size()<<endl;
+
             for (Fontanero i : solucion)
             {
-            cout<<"Cliente #"<<i.numCliente<<"\t Tiempo de Reparacion: "<< i.tiempo <<"\t Tiempo Espera: "<<i.llegar<<endl;
+                cout<<"Cliente #"<<i.numCliente<<"\t Tiempo de Reparacion: "<< i.tiempo <<"\t Tiempo Espera: "<<i.llegar;
+                if (i.llegar <= sati)
+                {
+                    cout<<"\t cliente satisfecho"<<endl;
+                    clientesSati++;   
+                }
+                else  if(i.llegar > sati && i.llegar <= sati+20 ) //los numeros son decididos por la empresa establecidos siempre 
+                {
+                    cout<<"\t cliente medio satisfecho"<<endl;
+                }
+                else
+                {
+                    cout<<"\t cliente insatisfecho"<<endl;
+                }
+                
+                
             }
 
             cout<<"Tiempo de la Jornada del fontanero: "<<jornada<<endl;
+            cout<<"Número de Clientes Satisfechos: "<<clientesSati<<endl;
         }
 
 };
@@ -68,7 +88,7 @@ int main()
     }
     sort(listaTrabajo.begin(), listaTrabajo.end(), tiempo); //tiempo busca la funcion bool tiempo :D 
 
-    Satisfaccion empresa;
+    Satisfaccion empresa(30);
     empresa.mejorarSatis(listaTrabajo);
 
     return 0;
